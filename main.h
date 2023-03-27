8921fb36b4b770d5de503ab7286d53c2a27b7595
Kathryn6368 <6368@holbertonstudents.com>
2023-03-26 19:41:36 -0700

    add _putchar prototype

--- main.h
+++ main.h
@@ -1,6 +1,13 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
+/* List of prototypes and their respective declaration files */
+
+/* 1-helper_functions.c */
 int _putchar(char c);
+
+/* 2-helper_functions.c */
+/* 3-helper_functions.c */
+/* 4-helper_functions.c */
 
 #endif /* __MAIN_H__ */
