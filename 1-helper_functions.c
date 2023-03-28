f744e059346e2438bcaebea0e15406f305cbbfb1
BryceKnight16 <6373@holbertonstudents.com>
2023-03-28 02:42:23 -0700

    code for the helper functions

--- 1-helper_functions.c
+++ 1-helper_functions.c
@@ -11,3 +11,46 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+void print_char(va_list ap)
+{
+	_putchar(va_arg(ap, char));
+}
+
+void print_str(va_list ap)
+{
+	char *str = va_arg(ap, char *);
+	int i = 0;
+
+	while (str != '\0')
+	{
+		_putchar(str[i]);
+		i = i + 1;
+	}
+}
+
+void print_int(va_list ap)
+
+{
+	int x = va_arg(ap, int);
+	int z = 0;
+	int i = 1000000000
+
+		while (i > 0)
+		{
+			if (i > x)
+			{
+				i = i / 10;
+			}
+			else
+			{
+				z = x - (x % i);
+				x = x - z;
+				z = z / i:
+				i = i / 10;
+				_putchar(z + '0');
+			}
+		}
+	putchar('\n');
+	return(0);
+}
