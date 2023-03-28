c023f108f4e0d4bb9cb983eadafe4c7f1eea8988
BryceKnight16 <6373@holbertonstudents.com>
2023-03-28 02:31:29 -0700

    adding helper functions and the struct of the header file

--- main.h
+++ main.h
@@ -3,10 +3,21 @@
 
 /* List of prototypes and their respective declaration files */
 
+typedef struct format_specs
+{
+	char *specifier; //eg "%s" "%c" "%i" "%d'.
+	void (*f)(va_list)
+} spec_t;
+
 /* 1-helper_functions.c */
 int _putchar(char c);
-
+void print_char(va_list ap);
+void print_str(va_list ap);
+void print_int(va_list ap);
+void print_decimal(va_list ap);
 /* 2-helper_functions.c */
+int _strlen(char *s);
+void (*get_fmat_spec(char *s))(va_list);
 /* 3-helper_functions.c */
 /* 4-helper_functions.c */
 
