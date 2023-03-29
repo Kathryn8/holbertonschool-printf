fa2450b815120f1315a0b691247ece878a6641da
Kathryn6368 <6368@holbertonstudents.com>
2023-03-28 19:32:48 -0700

    fix small typos

--- 1-helper_functions.c
+++ 1-helper_functions.c
@@ -1,4 +1,7 @@
 #include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
  * _putchar - writes the character c to stdout
@@ -14,15 +17,16 @@ int _putchar(char c)
 
 void print_char(va_list ap)
 {
-	_putchar(va_arg(ap, char));
+	_putchar(va_arg(ap, int));
 }
 
 void print_str(va_list ap)
 {
 	char *str = va_arg(ap, char *);
 	int i = 0;
+	printf("boo!");
 
-	while (str != '\0')
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i = i + 1;
@@ -30,27 +34,30 @@ void print_str(va_list ap)
 }
 
 void print_int(va_list ap)
-
 {
 	int x = va_arg(ap, int);
 	int z = 0;
-	int i = 1000000000
+	int i = 1000000000;
 
-		while (i > 0)
+	while (i > 0)
+	{
+		if (i > x)
 		{
-			if (i > x)
-			{
-				i = i / 10;
-			}
-			else
-			{
-				z = x - (x % i);
-				x = x - z;
-				z = z / i:
-				i = i / 10;
-				_putchar(z + '0');
-			}
+			i = i / 10;
 		}
-	putchar('\n');
-	return(0);
+		else
+		{
+			z = x - (x % i);
+			x = x - z;
+			z = z / i;
+			i = i / 10;
+			_putchar(z + '0');
+		}
+	}
+	_putchar('\n');
+}
+
+void print_decimal(va_list ap)
+{
+	_putchar('D');
 }
