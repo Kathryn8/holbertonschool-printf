2d58c1ee5bfa13c166d2a7d5eae88ae96ad29b1f
Kathryn6368 <6368@holbertonstudents.com>
2023-03-28 19:34:03 -0700

    adjust logic and while loops

--- print_format.c
+++ print_format.c
@@ -1,6 +1,14 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
+ * _printf - a function that prints char, string, integer and decimal formats
+ * @format: user input format string, may or may not contain format identifiers
  *
- *
+ * Return: void
  */
 
 int _printf(const char *format, ...)
@@ -8,28 +16,38 @@ int _printf(const char *format, ...)
 	va_list ap;
 	int i;
 	int j;
+	csid_t match[] = {
+		{'c', "%c"},
+		{'s', "%s"},
+		{'i', "%i"},
+		{'d', "%d"},
+		{'\0', NULL}
+	};
 
+	va_start(ap, format);
 	i = 0;
-	j = 0;
-
-
-	while (*format[i] != '\0')
+	while (format[i] != '\0') // loop over format string
 	{
+		j = 0;
+		while (match[j].c != '\0') // loop over the match struct
+		{
+			if ((format[i] == '%') && (format[i + 1] == match[j].c))
+			{
+				get_func(match[j].str)(ap); // prints variable
+				i = i + 2;
+				break;
+			}
+			else if ((format[i] == '%') && (format[i + 1] != match[j].c))
+			{
+				_putchar('%');
+				i = i + 1;
+				break;
+			}
+			else
+			j = j + 1;
+		}
 		_putchar(format[i]);
-
-		if (format[i] = '%')
-			   {
-				   if(format[i + 1] == "%c" || format[i + 1] == "%s")
-				   {
-					   get_func(format[i])(ap);
-				   }
-			   }
-
-
-
-
 		i = i + 1;
 	}
-	_putchar("N\n");
-	exit(121);
+	va_end(ap);
 }
